7bc191a1c4086dc39fbe09997df837eeda24d714
mat fournier <mat.fournier@visier.com>
2019-02-19 16:24:18 -0800

    feature complete, needs refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -11,11 +11,28 @@ import           System.Exit
 
 -- if the file has RCIs, gather all of them into a list
 -- otherwise return the empty list
-gatherJavaRcis :: B8.ByteString -> IO ([B8.ByteString])
-gatherJavaRcis filePath = do
+
+data FileWithRci = FileWithRci B8.ByteString [B8.ByteString] Bool
+
+extractFile :: FileWithRci -> B8.ByteString
+extractFile (FileWithRci f _ _) = f
+
+extractRcis :: FileWithRci -> [B8.ByteString]
+extractRcis (FileWithRci _ rcis _) = rcis
+
+fileIsRciDef :: FileWithRci -> Bool
+fileIsRciDef (FileWithRci _ _ b) = b
+
+whiteList :: Set.Set B8.ByteString
+whiteList = Set.fromList [B8.pack "RCI9999999"]
+
+gatherRciDefs :: B8.ByteString -> IO (FileWithRci)
+gatherRciDefs filePath = do
   file <- B8.readFile fp
   let lines' = B8.lines file
-  if (fileHasRci file) then return $ gatherRci lines' else return []
+  if (fileHasRci file)
+    then return $ FileWithRci filePath (gatherRci lines') True
+    else return $ FileWithRci filePath (gatherTargetRci lines') False
   where fp = B8.unpack filePath
         fileHasRci = B8.isInfixOf $ B8.pack "implements RootCause"
 
@@ -29,35 +46,19 @@ gatherRci lines' = concat maybeRcis
   maybeRcis =
     map ((map fst) . getRcis) lines'
 
--- maps over each file to gather all the rcis and <> them into one big list
--- returns: a list of RCIs that are duplicates
-gatherRciFromScala :: [B8.ByteString] -> Set.Set B8.ByteString -> IO [B8.ByteString]
-gatherRciFromScala scalaFiles rcis = do
-  scalaRcis <- mconcat <$> mapM gatherScalaRci scalaFiles
-  let dupes =  map fst (filter ((>=2) . snd) $ frequency scalaRcis)
-  return $ filter (flip Set.member $ rcis) dupes
-  where
-     -- use a map to store list elements and their count and convert back to a list
-    frequency :: (Ord a) => [a] -> [(a, Int)]
-    frequency xs = Map.toList (Map.fromListWith (+) [ (x, 1) | x <- xs ])
+gatherTargetRci :: [B8.ByteString] -> [B8.ByteString]
+gatherTargetRci lines' = gatherRci validLines
+  where validLines = filter (\l -> (not $ isImport l) && (not $ isComment l)) lines'
+          where isImport l = B8.isPrefixOf (B8.pack "import") l
+                isComment l = B8.isPrefixOf "* " stripped || B8.isPrefixOf "//" stripped
+                  where stripped = B8.dropWhile (== ' ') l
 
-gatherScalaRci :: B8.ByteString -> IO [B8.ByteString]
-gatherScalaRci filePath = do
-  file <- B8.readFile $ fp
-  let lines' =
-        filter (\l -> (not $ isImport l) && (not $ isComment l)) $ B8.lines file
-  return $ gatherRci lines'
- where
-  fp = B8.unpack filePath
-  isImport l = B8.isPrefixOf (B8.pack "import") l
-  isComment l = B8.isPrefixOf "* " stripped || B8.isPrefixOf "//" stripped
-    where stripped = B8.dropWhile (== ' ') l
-
-endsWithScala :: B8.ByteString -> Bool
-endsWithScala l = l =~ [re|\.scala\n?|]
-
-endsWithJava :: B8.ByteString -> Bool
-endsWithJava l = l =~ [re|\.java\n?|]
+countRciDupes :: [FileWithRci] -> Set.Set B8.ByteString -> [B8.ByteString]
+countRciDupes targetFiles rcis = filter (flip Set.member $ rcis) dupes
+  where extractedRcis = concat $ map extractRcis targetFiles
+        frequency :: (Ord a) => [a] -> [(a, Int)]
+        frequency xs = Map.toList (Map.fromListWith (+) [ (x, 1) | x <- xs ])
+        dupes =  map fst (filter ((>=2) . snd) $ frequency extractedRcis)
 
   -- need to filter out files with "/test/" and "/it/"
 isTestFilePath :: B8.ByteString -> Bool
@@ -70,18 +71,20 @@ isTestFilePath filePath =
 main :: IO ()
 main = do
   input <- B8.getContents
-  let files     = B8.lines input
-  let javaFiles = filter endsWithJava files
-  rcis <- mapM gatherJavaRcis javaFiles
-  let scalaFiles =
-        filter (\fp -> endsWithScala fp && (not $ isTestFilePath fp)) files
-  let setRcis = Set.fromList $ concat rcis
-  duplicates <- gatherRciFromScala scalaFiles setRcis
-  if (null duplicates)
+  let files = B8.lines input
+  filesAndRciDefs <-  mapM gatherRciDefs files
+
+  let rciDefFiles = filter fileIsRciDef filesAndRciDefs
+  let setRcis = Set.fromList $ concat (map extractRcis rciDefFiles)
+
+  let targetFiles = filter (\f -> not (isTestFilePath (extractFile f)) && not (fileIsRciDef f)) filesAndRciDefs
+  let duplicates = countRciDupes targetFiles setRcis
+  let whiteListedDuplicates = filter (flip Set.notMember $ whiteList) duplicates
+  if (null whiteListedDuplicates)
     then do
       putStrLn $ "No duplicate RCIs"
     else do
       putStrLn $ "Duplicate RCIs:\n" <> intercalate
         "\n"
-        (map (B8.unpack) duplicates)
+        (map (B8.unpack) whiteListedDuplicates)
       exitFailure
